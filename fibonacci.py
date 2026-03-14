8ca5de5c89edb21c2bedaf4e907d03e54444aae8
RKZ <136750473+rickrickoso@users.noreply.github.com>
2026-03-14 20:15:04 -0300

    Detalhando comentários de exercícios

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,9 @@
+#SEQUENCIA DE FIBONACCI N+1 = (N-1) + (N-2)
 a=0
 b=1
 
-n = int(input("Digite o número até onde vai: "))
+n = int(input("Digite o número até onde vai: ")) #USUARIO ESCOLHE ATÉ ONDE VAI A SEQUENCIA
 while b <= n:
     print (b)        
-    a, b = b, a+b    # atualiza os valores
+    a, b = b, a+b    #ATUALIZA OS VALORES, FAZENDO A CONTA A -> B / B -> A + B 
     
