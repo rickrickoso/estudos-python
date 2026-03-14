980cf1adc82eb87ed0fef3232afefaa47a9fbe06
RKZ <136750473+rickrickoso@users.noreply.github.com>
2026-03-14 20:32:19 -0300

    Detalhando comentários de exercícios

--- fizzbuzz_dicionario.py
+++ fizzbuzz_dicionario.py
@@ -1,13 +1,12 @@
 #FIZZBUZZ C/ DICIONARIO
-d = {(True, True): "FizzBuzz", (True, False):"Fizz", (False, True):"Buzz"}
-for i in range (1,51):   
-    t = (i % 3 == 0, i % 5 == 0)
-    r = d.get(t, i)
+d = {(True, True): "FizzBuzz", (True, False):"Fizz", (False, True):"Buzz"}         #DICIONARIO DE RESULTADOS
+for i in range (1,51):                                                             #ITERA DE 1 ATÉ 50
+    t = (i % 3 == 0, i % 5 == 0)                                                   #TUPLA DE BUSCA DE RESULTADOS
+    r = d.get(t, i)                                                                #BUSCA A TUPLA DENTRO DO DICIONÁRIO, OU O CONTADOR DA ITERAÇÃO QUANDO NÃO ENCONTRAR
     print(r)
         
 """
 dicionario = {"chave":"valor","chave1":"valor1"}
-0
 print(dicionario.get("chave")) 
 
 """
