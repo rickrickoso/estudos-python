8c2beab6532099be075e43286909d9ca72ed26ca
RKZ <136750473+rickrickoso@users.noreply.github.com>
2026-03-14 20:17:23 -0300

    Detalhando comentários de exercícios

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,12 @@
-# FizzBuzz
+#FIZZBUZZ
 
-for i in range (1,51):                          #itera até 50
-    if (i % 3 == 0) and (i % 5 == 0)== True:    #verifica se o numero é divisivel por 3 e por 5
+for i in range (1,51):                          #ITERA ATÉ 50
+    if (i % 3 == 0) and (i % 5 == 0)== True:    #VERIFICA SE O NUMERO E DIVISIVEL POR 3 E POR 5 AO MESMO TEMPO
         print ("FizzBuzz")
-    elif i % 3 == 0:                            #verifica se o numero é divisivel por 3
+    elif i % 3 == 0:                            #VERIFICA SE O NUMERO E DIVISIVEL POR 3
         print ("Buzz")      
     elif i % 5 == 0:
-        print("Fizz")                           #verifica se o numero é divisivel por 5
+        print("Fizz")                           #VERIFICA SE O NUMERO E DIVISIVEL POR 5
     else:
         print(i)
 
