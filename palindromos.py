72065e3d479709a0c05d064b72d34c6e505e0566
RKZ <136750473+rickrickoso@users.noreply.github.com>
2026-03-16 11:14:41 -0300

    Detalhando comentários

--- palindromos.py
+++ palindromos.py
@@ -1,13 +1,13 @@
-# Palindrome Detector
+#DETECTOR DE PALINDROMO
 
-print("DETECTOR DE PALINDROMO (PALAVRA IGUAL DE TRAZ PRA FRENTE)") #saudacao
+print("DETECTOR DE PALINDROMO (PALAVRA IGUAL DE TRAZ PRA FRENTE)")
 
-word = input("Digite a palavra: ") #recebe o possivel palindromo
+word = input("Digite a palavra: ") #RECEBE A PALAVRA
 
-if word == word[::-1]:              #var[::-1] == rav [é o slicing da string] 
-    print("É um palindromo!!")      #resposta positiva
+if word == word[::-1]:              #UTILIZANDO SLICING, TESTAMOS A STRING DE TRAZ PARA FRENTE (var[::-1] == rav) 
+    print("É um palindromo!!")      #RESPOSTA POSITIVA
 else:
-    print ("Não é palindromo...")   #resposta negativa
+    print ("Não é palindromo...")   #RESPOSTA NEGATIVA
 
     
     
