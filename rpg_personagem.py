2abd263980d8df6648dccbfbadcb6c0d04d9e833
RKZ <136750473+rickrickoso@users.noreply.github.com>
2026-03-16 11:19:04 -0300

    Detalhando comentários

--- rpg_personagem.py
+++ rpg_personagem.py
@@ -8,12 +8,12 @@ atributoF = int(input("Digite sua força(0-5): "))
 atributoA = int(input("Digite sua agilidade(0-5): "))
 atributoI = int(input("Digite sua intelecto(0-5): "))
 print("\nSEU INVENTÁRIO ESTÁ VAZIO! ESCOLHA DOIS ITENS\n")
-inventario = [input("Digite o primeiro item: "), input("Digite o segundo item: ")]
+inventario = [input("Digite o primeiro item: "), input("Digite o segundo item: ")]        #RECEBE OS DADOS DO PERSONAGEM
 
-personagem = {
+personagem = {                                                                            #DICIONÁRIO DE PERSONAGEM
     "nome": nome,
     "especie": especie,
-    "atributos": {
+    "atributos": {                                                                        #SUBDICIONÁRIO DE ATRIBUTOS
         "forca": atributoF,
         "agilidade": atributoA,
         "intelecto": atributoI
@@ -21,13 +21,13 @@ personagem = {
     "inventario": inventario
     }
 
-for chave, valor in personagem.items():
-    if isinstance(valor,dict):
+for chave, valor in personagem.items():                                                    #ITERA NOS ITENS DO DICIONÁRIO
+    if isinstance(valor,dict):                                                             #UTILIZA O isinstance DO PYTHON PARA VERIFICAR SE É UM SUBDICIONÁRIO.
         print(f"{chave.title()}")
-        for sub_chave, sub_valor in valor.items():
+        for sub_chave, sub_valor in valor.items():                                         #ITERA NOS ITENS DO SUBDICIONÁRIO
             print(f" -> {sub_chave.title()}:{sub_valor}")
     else:
-        print(f"{chave.title()}:{valor}")
+        print(f"{chave.title()}:{valor}")                                                  #IMPRIME 
     
 
 
